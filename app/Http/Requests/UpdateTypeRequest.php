6cc232949cecfebdbbfaeadce76f76d4f168d19f
CraciDenisStefan <crdenis13@gmail.com>
2023-08-24 18:13:19 +0200

    Request ok / Esercizio + Bonus finito

--- app/Http/Requests/UpdateTypeRequest.php
+++ app/Http/Requests/UpdateTypeRequest.php
@@ -24,7 +24,18 @@ class UpdateTypeRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => 'required|max:50',
+            'image_type' => 'image|mimes:jpeg,png,jpg,gif,svg|max:250',
+        ];
+    }
+    public function messages(){
+        return[
+
+            'name.required' => 'Il nome è obbligatorio !',
+            'name.max' =>  'Numero massimo di caratteri nel titolo è :max !',
+            'image_type.image' =>'Il file deve essere un\'immagine!',
+            'image_type.mimes' => 'Il file immagine deve essere in uno dei seguenti formati: jpeg, jpg, png, gif',
+            'image_type.max' => 'Il file immagine non deve superare :max KB!',
         ];
     }
 }
